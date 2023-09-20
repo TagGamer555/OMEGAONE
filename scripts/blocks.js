28085a9ca21edcfa904cf4def328b3973f3ed560
TagGamer555 <69044271+TagGamer555@users.noreply.github.com>
2023-09-20 21:09:05 +0100

    yyy

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -2,4 +2,5 @@
 	//region - turrets
 	const conductor = extend(PowerTurret, "conductor", {});
 	const beamTesla = extend(PowerTurret, "beam-tesla", {});
+	const nailgun = extend(ItemTurret, "nailgun", {});
 }
