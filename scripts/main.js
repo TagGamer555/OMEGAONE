d1cf994c2307b423d70d3983d098dd63fd84edb1
TagGamer555 <69044271+TagGamer555@users.noreply.github.com>
2023-09-20 17:53:40 +0100

    a

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,8 @@
   
 const mod = Vars.mods.locateMod("Ω1");
-mod.meta.displayName = "[#EDA666]Ω1[]";
-mod.meta.description = "DISCORD: https://discord.gg/sGn9hvPgEE (join!)\n\nState: INDEV\nContent: NONE\nPlayable: TRUE";
-mod.meta.author = "[lime]KillerIKY - lead dev";
 
-require("blocks/walls");
-require("blocks/power");
-require("blocks/liquid");
-require("blocks/distribution");
-require("blocks/projectors");
-require("blocks/storage");
-require("blocks/environment");
-require("blocks/production");
-require("blocks/drills");
-require("blocks/turrets");
-require("blocks/payload-blocks");
-require("statuses/statuses");
+require("blocks");
 require("units");
-require("blocks/erekir-content");
 require("liquids");
 require("weathers");
 require("items");
