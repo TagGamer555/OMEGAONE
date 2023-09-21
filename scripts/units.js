41405734a82fa1bd501f1ba11d8316a101714246
TagGamer555 <69044271+TagGamer555@users.noreply.github.com>
2023-09-21 15:05:24 +0100

    sheesh

--- scripts/units.js
+++ scripts/units.js
@@ -1,5 +1,4 @@
 {
 	//region - misc
-	const gladiatorUnit = extend(UnitType, "gladiator-unit", {});
-	gladiatorUnit.constructor = () => extend(UnitEntity, {});
+	const gladiatorUnit = extend("gladiator-unit", {});
 }
