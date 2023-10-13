1093934ef1fa1f4a993bd62ddaed3179e3612888
TagGamer555 <69044271+TagGamer555@users.noreply.github.com>
2023-10-13 22:50:29 +0100

    oopsies

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -3,6 +3,7 @@
 	const conductor = extend(PowerTurret, "conductor", {});
 	const beamTesla = extend(PowerTurret, "beam-tesla", {});
 	const nailgun = extend(ItemTurret, "nailgun", {});
+	const score = extend(PowerTurret, "score", {});
 	//region - crafting
 	const steelSmelter = extend(GenericCrafter, "steel-smelter", {});
 	const ironCaster = extend(HeatCrafter, "iron-caster", {});
