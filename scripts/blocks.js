c83ba5b7cb522fec55b3658824990a1248acd92f
TagGamer555 <69044271+TagGamer555@users.noreply.github.com>
2023-10-13 22:04:25 +0100

    omfg yes pleaes

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -3,5 +3,7 @@
 	const conductor = extend(PowerTurret, "conductor", {});
 	const beamTesla = extend(PowerTurret, "beam-tesla", {});
 	const nailgun = extend(ItemTurret, "nailgun", {});
+	//region - crafting
 	const steelSmelter = extend(GenericCrafter, "steel-smelter", {});
+	const ironCaster = extend(GenericCrafter, "iron-caster", {});
 }
