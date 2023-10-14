f6e359cceee035182ec44454eb99ec7d2e2ee79f
TagGamer555 <69044271+TagGamer555@users.noreply.github.com>
2023-10-14 23:10:25 +0100

    heat pipes! :D

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -9,6 +9,8 @@
 	const ironCaster = extend(HeatCrafter, "iron-caster", {});
 	//region - heat
 	const combustionHeater = extend(HeatProducer, "combustion-heater", {});
+	const heatPipe = extend(HeatConductor, "heat-pipe", {});
+	const heatPipeRouter = extend(HeatConductor, "heat-pipe-router", {});
 	//region - defense
 	const advancedSurgeWall = extend(PowerTurret, "advanced-surge-wall", {});
 	const advancedSurgeWallLarge = extend(PowerTurret, "advanced-surge-wall-large", {});
